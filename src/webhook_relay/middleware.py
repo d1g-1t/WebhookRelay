2150e740f9098c6660d9242bb6e2257d4ff77920
d1g-1t <paokhrim@gmail.com>
2026-03-14 08:57:34 +0300

    Add RequestIDMiddleware to track request IDs and response times

---
+++ src/webhook_relay/middleware.py
@@ -0,0 +1,17 @@
+import time
+import uuid
+
+from fastapi import Request, Response
+from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class RequestIDMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        request_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
+        request.state.request_id = request_id
+        start = time.perf_counter()
+        response: Response = await call_next(request)
+        duration = round((time.perf_counter() - start) * 1000, 2)
+        response.headers["X-Request-ID"] = request_id
+        response.headers["X-Response-Time-Ms"] = str(duration)
+        return response
